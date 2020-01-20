f7a0ef894f4dd3b23107fc165d2fd0217706632f
davidjimenez0 <djimenez0315@gmail.com>
2020-01-20 12:06:01 -0500

    first commit

---
+++ src/main/java/com/david/app/fullspringapp/FullSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.david.app.fullspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/app/fullspringapp/FullSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.app.fullspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
