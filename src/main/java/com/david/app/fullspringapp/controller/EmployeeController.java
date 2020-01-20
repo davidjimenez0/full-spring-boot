c9b7fe3cfdcf7762cd5c42fcb36da7378d504591
davidjimenez0 <djimenez0315@gmail.com>
2020-01-20 12:56:27 -0500

    add exception

--- src/main/java/com/david/app/fullspringapp/controller/EmployeeController.java
+++ src/main/java/com/david/app/fullspringapp/controller/EmployeeController.java
@@ -1,4 +1,50 @@
 package com.david.app.fullspringapp.controller;
 
+
+import com.david.app.fullspringapp.data.EmployeeRepository;
+import com.david.app.fullspringapp.domain.Employee;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class EmployeeController {
+
+    private final EmployeeRepository repository;
+
+    EmployeeController(EmployeeRepository employeeRepository){
+        this.repository = employeeRepository;
+    }
+
+    @GetMapping("/employees")
+    List<Employee> all(){
+        return repository.findAll();
+    }
+
+    @PostMapping("/employees/{id}")
+    Employee newEmployee(@RequestBody Employee newEmployee){
+        return repository.save(newEmployee);
+    }
+
+    @GetMapping("employees/{id}")
+    Employee one(@PathVariable Long id){
+        return repository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+    }
+
+    @PutMapping("/employees/{id}")
+    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id){
+
+        return repository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setRole(newEmployee.getRole());
+                    return repository.save(employee);
+                })
+                .orElseGet(() -> {
+                    newEmployee.setId(id);
+                    return repository.save(newEmployee);
+                });
+    }
+
 }
